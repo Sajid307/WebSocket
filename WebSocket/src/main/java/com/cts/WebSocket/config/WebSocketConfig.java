8d56bdbf9dba51b7929dd9d2ad5ae7b9cd9fc331
Sajid307 <88498205+Sajid307@users.noreply.github.com>
2024-04-23 12:40:43 +0530

    Add all files

---
+++ WebSocket/src/main/java/com/cts/WebSocket/config/WebSocketConfig.java
@@ -0,0 +1,7 @@
+package com.cts.WebSocket.config;
+
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+}
